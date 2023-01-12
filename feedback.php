347535fbd397f13ebf0d1e5bb7df85aff17a28b2
Greeshma <36813587+greeshmaganji@users.noreply.github.com>
2023-01-12 13:38:44 +0530

    Add files via upload

---
+++ feedback.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<body style="background-color: #BCDFFF;text-align:center; margin-top:100px; font-size: xx-large;">
+<?php
+echo "Thank you for submitting the feedback ";
+echo $_GET["name"];
+echo "!";
+?>
+</body>
+<html>
